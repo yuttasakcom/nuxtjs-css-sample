8d206d1e47fa2f564bd6f8f806cf0477feea2acc
Yuttasak Pannawat <yuttasakcom@gmail.com>
2018-03-27 17:17:03 +0700

    init project

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header class="main-header">
+      <div>
+          <a href="" class="main-header__brand">uHost</a>
+        </div>
+      <nav class="main-nav">
+        <ul class="main-nav__items">
+          <li class="main-nav__item"><a href=""> Package </a></li>
+          <li class="main-nav__item"><a href=""> Customers </a></li>
+          <li class="main-nav__item"><a href=""> Start Hosting </a></li>
+        </ul>
+      </nav>
+    </header>
+    <section id="product-overview">
+      <h1>Get the freedom you deserve.</h1>
+    </section>
+    <section id="plan">
+      <h1 class="section-title">Choose Your Plan</h1>
+      <p>Make sure you get the most for your money!</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  head: () => ({
+    title: "uHost"
+  })
+};
+</script>
